480a4dbeb3c0fa0b3abaccef7c8a34028c1d5719
Phuong Nguyen The <phuong.nt@amanotes.com>
2020-10-29 18:37:47 +0700

    Carthage support

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Unrealm' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'RuntimeNew', :git => "https://github.com/arturdev/RuntimeNew.git"
+  pod 'RealmSwift', '5.0.3'
+end
---
+++ Unrealm/Unrealm.h
@@ -0,0 +1,18 @@
+//
+//  Unrealm.h
+//  Unrealm
+//
+//  Created by Nguyen The Phuong on 10/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Unrealm.
+FOUNDATION_EXPORT double UnrealmVersionNumber;
+
+//! Project version string for Unrealm.
+FOUNDATION_EXPORT const unsigned char UnrealmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unrealm/PublicHeader.h>
+
+
